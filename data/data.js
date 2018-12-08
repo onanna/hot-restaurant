7c72e13a354f1e27569cfaeb8b9a1b75630fca6b
Ulyses <ulysesacolon@gmail.com>
2018-12-08 11:30:23 -0500

    ulyses res

---
+++ data/data.js
@@ -0,0 +1,9 @@
+// variables for the reservation and waitlist data
+// =============================================================
+
+var reservation = [{
+    name: "Ulyses",
+    phonenumber: "8675309",
+    email: "awesome.gmail.com",
+    unique: "1234"
+}]
