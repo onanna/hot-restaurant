0360caa4f311150839ac2534618725f13e3a5279
JayBee <jemallbrown@gmail.com>
2018-12-08 11:31:26 -0500

    adding link for json data in routes.js file

--- route/routes.js
+++ route/routes.js
@@ -19,3 +19,8 @@ module.exports = function(app){
       res.sendFile(path.join(__dirname + '/../index.html'));
   });
 };
+
+// Displays all current reservations
+app.get("/data/reservation", function(req, res) {
+    return res.json(reservation);
+  });
